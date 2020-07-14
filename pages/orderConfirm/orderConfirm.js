455adbe12fe2ef31ef2d514dc71c2359d73d5561
gxx <1152862327@qq.com>
2020-07-15 07:42:24 +0800

    add confirm_totalPrice、totalNum

--- pages/orderConfirm/orderConfirm.js
+++ pages/orderConfirm/orderConfirm.js
@@ -9,6 +9,111 @@ Page({
     numberOfDiners: -1,
     tabTypeName: "",
     tableName: "",
+
+    items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+    confirmModalStatus: false,
+    totalPrice: 0,
+    totalNum: 0,
+    menuForNum: [],
+
+    orders: [],
+    userImage: '',
+    userNickName: ''
+  },
+  /**
+   * 根据outIndex、index判断菜品是否有属性和规格
+   */
+  hasProOrSpec: function (outIndex, index) {
+    var app = getApp();
+    var food = app.data.menu[outIndex].foods[index];
+    if(food.property.length>0||food.specs.length>0){
+      return true;
+    } else {
+      return false;
+    }
+  },
+  //计算总数量
+  countTotalPrice: function () {
+    var totalPrice2 = 0;
+    for(var i=0; i<this.data.menuForNum.length; i++) {
+      for(var j=0; j<this.data.menuForNum[i].length; j++) {
+        if(this.hasProOrSpec(i, j)) {
+          var intotalPrice = 0;
+          for(var k=1; k<this.data.menuForNum[i][j].length; k++) {
+            intotalPrice += this.data.menuForNum[i][j][k].num * this.data.menuForNum[i][j][k].price;
+          }
+          totalPrice2 += intotalPrice;
+        } else {
+          totalPrice2 += this.data.menuForNum[i][j][0].num * this.data.menuForNum[i][j][0].price;
+        }
+      }
+    }
+    this.setData({
+      totalPrice: totalPrice2
+    });
+  },
+  //计算总价
+  countTotalNum: function() {
+    var totalNum2 = 0;
+    for(var i=0; i<this.data.menuForNum.length; i++) {
+      for(var j=0; j<this.data.menuForNum[i].length; j++) {
+        totalNum2+=this.data.menuForNum[i][j][0].num;
+      }
+    }
+    this.setData({
+      totalNum: totalNum2
+    })
+  },
+  //单选框选择
+  radioChange: function (e) {
+    this.setData({
+      numberOfDiners: e.detail.value
+    });
+    var app = getApp();
+    app.data.numberOfDiners = this.data.numberOfDiners;
+    this.confirmHideModal();
+  },
+  //隐藏弹窗
+  confirmHideModal: function () {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    });
+    this.animation = animation;
+    animation.scale3d(0,0,0).step()
+    this.setData({
+      confirmAnimationData: animation.export(),
+    });
+    setTimeout(function () {
+      animation.scale3d(1,1,1).step()
+      this.setData({
+        confirmAnimationData: animation.export(),
+        confirmModalStatus: false
+      })
+    }.bind(this), 200)
+  },
+  //显示弹窗
+  confirmShowModal: function() {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation;
+    animation.scale3d(0,0,0).step()
+    this.setData({
+      confirmAnimationData: animation.export(),
+      confirmModalStatus: true
+    })
+    setTimeout(function () {
+      animation.scale3d(1,1,1).step()
+      this.setData({
+        confirmAnimationData: animation.export()
+      })
+    }.bind(this), 200)
   },
 
   /**
@@ -18,6 +123,13 @@ Page({
     //根据app的res和table，请求得tabTypeName，和tabName
     let that = this;
     var app = getApp();
+    this.setData({
+      numberOfDiners: app.data.numberOfDiners,
+      menuForNum: app.data.menuForNum,
+      orders: app.data.orders
+    });
+    this.countTotalPrice();
+    this.countTotalNum();
     //请求要携带openid
     wx.request({
       url: app.data.realUrl + '/wechat/loggedIn/getTabNameAndTabTypeName',
@@ -35,7 +147,6 @@ Page({
         });
       }
     });
-    console.log("test git");
   },
 
   /**
